1437070ab6658418861ff6afa1268cd625f68ecd
lukeeskinner <lukeskinner101@gmail.com>
2025-12-28 12:03:56 -0700

    Initial Commit

---
+++ RecoveryLens/RecoveryLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecoveryLensApp.swift
+//  RecoveryLens
+//
+//  Created by Luke Skinner on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct RecoveryLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
